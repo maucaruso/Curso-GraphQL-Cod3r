b468974870db877d7cd68b40c384bf88ee3d067c
maurici3 <carusojr@outlook.com>
2022-03-25 20:59:41 -0300

    feact: resolver examples

--- schema-query/projeto/projeto/index.js
+++ schema-query/projeto/projeto/index.js
@@ -1,21 +1,67 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const typeDefs = gql`
-  # Pontos de entrada da sua API!
+  scalar Date 
+
+  type Usuario {
+    id: ID!
+    nome: String!
+    email: String!
+    idade: Int
+    salario: Float
+    vip: Boolean
+  }
+
   type Query {
-    ola: String,
-    horaCerta: String
+    ola: String!,
+    horaCerta: Date!,
+    usuarioLogado: Usuario,
+    produtoEmDestaque: Produto
+  }
+
+  type Produto {
+    nome: String!,
+    preco: Float!,
+    desconto: Float,
+    precoComDesconto: Float
   }
 
 `;
 
 const resolvers = {
+  Usuario: {
+    salario(usuario) {
+      return usuario.salario_real
+    },
+  },
+  Produto: {
+    precoComDesconto(produto) {
+      return produto.desconto ? produto.preco - (produto.preco * (produto.desconto / 100))  : null ;
+    }
+  },
   Query: {
     ola() {
       return 'Olá Mundo'
     },
     horaCerta() {
       return String(new Date())
+    },
+    usuarioLogado() {
+      return {
+        id: 1,
+        nome: 'gabi',
+        email: 'gabi@gmail.com',
+        idade: 23,
+        salario_real: 1200.00,
+        vip: true
+      }
+    },
+    produtoEmDestaque() {
+      return {
+        nome: 'Secador de cabelos',
+        preco: 100.00,
+        desconto: 10
+      }
     }
   }
 }
